c1834b02ad950fd36f7aed26625e32a3bec2fcec
Hermas Francisco <franciscohermas@gmail.com>
2026-01-22 14:14:56 +0100

    Fix: Correct views path for Netlify serverless environment

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,10 @@
 const serverless = require('serverless-http');
-const app = require('../../app.js'); 
+const app = require('../../app.js');
+
+const path = require('path');
+
+const viewsPath = path.join(process.cwd(), 'views'); 
+
+app.set('views', viewsPath);
 
 module.exports.handler = serverless(app);
