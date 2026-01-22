2abe3616b6625407cc4f8bd1ac6b9e16145c0e40
Hermas Francisco <franciscohermas@gmail.com>
2026-01-22 14:21:54 +0100

    "Fix: Define express in api.js wrapper"

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,12 @@
 const serverless = require('serverless-http');
 const app = require('../../app.js');
 const path = require('path');
+const express = require('express');
 
 const viewsPath = path.join(process.env.PWD, 'views'); 
-
 const publicPath = path.join(process.env.PWD, 'public');
-app.use(express.static(publicPath));
 
+app.use(express.static(publicPath));
 app.set('views', viewsPath);
 
 module.exports.handler = serverless(app);
