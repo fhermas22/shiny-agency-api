2d1a14644b78803e474541c5c32a20b9c511d074
Hermas Francisco <franciscohermas@gmail.com>
2026-01-22 14:31:57 +0100

    Fix: Add postbuild script to copy views/public folders to functions directory

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,10 +3,10 @@ const app = require('../../app.js');
 const path = require('path');
 const express = require('express');
 
-const viewsPath = path.join(process.env.PWD, 'views'); 
-const publicPath = path.join(process.env.PWD, 'public');
+const viewsPath = path.join(__dirname, 'views'); 
+const publicPath = path.join(__dirname, 'public');
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); 
 app.set('views', viewsPath);
 
 module.exports.handler = serverless(app);
