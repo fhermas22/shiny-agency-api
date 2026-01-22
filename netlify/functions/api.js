6e78a94ff8e425d092ba280e81163d8fee848424
Hermas Francisco <franciscohermas@gmail.com>
2026-01-22 14:18:57 +0100

    Fix: Use Netlify PWD variable for reliable absolute paths

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,11 @@
 const serverless = require('serverless-http');
 const app = require('../../app.js');
-
 const path = require('path');
 
-const viewsPath = path.join(process.cwd(), 'views'); 
+const viewsPath = path.join(process.env.PWD, 'views'); 
+
+const publicPath = path.join(process.env.PWD, 'public');
+app.use(express.static(publicPath));
 
 app.set('views', viewsPath);
 
